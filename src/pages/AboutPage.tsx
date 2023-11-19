fcb371a25c4ff15a5de6c4b9539becc0fe335e49
Sardor Madaminov <sardor0968@gmail.com>
2023-11-20 01:42:34 +0900

    page components: added

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,11 +1,28 @@
-export default function HomePage() {
+import React from 'react';
+import {Link} from "react-router-dom";
+import runnerHero from "../assets/images/runner-hero.jpg";
+export default function AboutPage() {
     return (
-        <div className="container">
-            <h1>Home Page</h1>
+        <div className="about-page-container">
+            <img
+                alt="runner-hero"
+                src={runnerHero} className="about-hero-image"/>
+            <div className="about-page-content">
+            <h1>
+               Start running with the community of runners.
+            </h1>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quisquam, quos.
+                Dont wait at home thinking about when to run or where to run or with whom to run with. Just run here with the people you want to run with.
+                Join the community of runners and find your running buddy. It is never late to start running. Just run here. :)
+            </p>
+            <p>
+                Our mission is to help people to find their running buddy and start running with them. Start your journey with us here and be the part of the community.
             </p>
         </div>
-    )
+        <div className="about-page-cta">
+            <h2> A community of runners are waiting for you. Join now.</h2>
+            <Link className="link-button" to={"/register"}>Join Now</Link>
+        </div>
+        </div>
+    );
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
-export default function Homepage(){
+import {Link} from "react-router-dom";
+
+export default function Homepage() {
     return (
-        <div className="container">
-            <h1>Home Page</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quisquam, quos.
-            </p>
+        <div className="home-container">
+           <h1>
+               If you want to run, run a mile with us. If you want to experience a different life, run a marathon with a community.
+           </h1>
+            <p> Add one mile everyday to your daily streak of running challenge and be part of the running community. </p>
+            <Link to="/register"> Find your buddy</Link>
         </div>
     )
 }
