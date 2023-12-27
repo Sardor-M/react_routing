edffc8c1a96ce667614e8200ef9aeba1746e5eae
Sardor Madaminov <sardor0968@gmail.com>
2023-12-28 01:06:18 +0900

    minor changes: added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,25 +2,13 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link to="/home" className="navbar-brand">
-        #Runhere
+    <header>
+      <Link className="site-logo" to="/">
+        #VanLife
       </Link>
-      <div className="container-fluid">
-        <a href="navbar" className="navbar-brand">
-          Navbar
-        </a>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            {/*<a className="nav-link active" aria-current="page" href="/">Home</a>*/}
-            {/*<a className="nav-link" href="/about">About</a>*/}
-            {/*<a className="nav-link" href="/login">Login</a>*/}
-            {/*<a className="nav-link" href="/register">Register</a>*/}
-            {/*<a className="nav-link" href="/dashboard">Dashboard</a>*/}
-            {/*<a className="nav-link" href="/logout">Logout</a>*/}
-          </div>
-        </div>
-      </div>
-    </nav>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </header>
   );
 }
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,23 +1,21 @@
-import {BrowserRouter as Router, Route, Routes,} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import HomePage from "../pages/HomePage";
 import AboutPage from "../pages/AboutPage";
 import ContactPage from "../pages/ContactPage";
-import App from "../App";
-import MainPage from "../pages/MainPage";
 
 export default function AppRoutes() {
-    return (
-        <Router>
-            <Navbar/>
-            <Routes>
-                <Route path="/" element={<MainPage/>}/>
-                <Route path="/home" element={<HomePage/>}/>
-                <Route path="/about" element={<AboutPage/>}/>
-                <Route path="/contact" element={<ContactPage/>}/>
-            </Routes>
-            <Footer/>
-        </Router>
-    )
-};
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<AboutPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
