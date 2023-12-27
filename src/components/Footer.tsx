e93ab598308fcf5de766a2bc9b380c8003261eb8
Sardor Madaminov <sardor0968@gmail.com>
2023-12-28 00:44:47 +0900

    code formating: added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,26 @@
-
-export default function Footer(){
-    return (
-        <footer className="bg-light text-center text-lg-start">
-            <div className="text-center p-3" style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}>
-                © 2023 by Sardor Madaminov:
-                <a className="text-dark" href="https://www.instagram.com/sardormd/">@sardor_md</a> |
-                <a className="text-dark" href="https://www.linkedin.com/in/sardor-madaminov/">LinkedIn</a> |
-                <a className="text-dark" href="https://www.sardor-m.dev/">sardor-m.dev</a>
-            </div>
-        </footer>
-    )
+export default function Footer() {
+  return (
+    <footer className="bg-light text-center text-lg-start">
+      <div
+        className="text-center p-3"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+      >
+        © 2023 by Sardor Madaminov:
+        <a className="text-dark" href="https://www.instagram.com/sardormd/">
+          @sardor_md
+        </a>{" "}
+        |
+        <a
+          className="text-dark"
+          href="https://www.linkedin.com/in/sardor-madaminov/"
+        >
+          LinkedIn
+        </a>{" "}
+        |
+        <a className="text-dark" href="https://www.sardor-m.dev/">
+          sardor-m.dev
+        </a>
+      </div>
+    </footer>
+  );
 }
