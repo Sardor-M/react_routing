53b13d3d15938ca102fae2c035e9b6f996eddf76
Sardor Madaminov <sardor0968@gmail.com>
2024-01-11 15:55:51 +0900

    minor changes to homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Slider, { CustomArrowProps } from "react-slick";
 import { Link } from "react-router-dom";
+import "../carousel.css";
 
 interface SlideData {
   image: string;
@@ -46,7 +47,15 @@ const Homepage: React.FC = () => {
   const slideData: SlideData[] = [
     {
       image:
-        "https://images.unsplash.com/photo-1517849845537-4d257902454a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmVhbCUyMGV2ZW50c3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+        "https://images.unsplash.com/photo-1682695796497-31a44224d6d6?q=80&w=3570&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Run with us",
+      description:
+        "If you want to run, run a mile with us. If you want to experience a different life, run a marathon with a community.",
+      date: "04/01/2023",
+    },
+    {
+      image:
+        " https://images.unsplash.com/photo-1695264424367-a61e7988200b?q=80&w=3570&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Run with us",
       description:
         "If you want to run, run a mile with us. If you want to experience a different life, run a marathon with a community.",
@@ -57,8 +66,7 @@ const Homepage: React.FC = () => {
     dots: true,
     infinite: true,
     speed: 500,
-    autoplay: true,
-    autoplaySpeed: 2000,
+    autoplay: false,
     slidesToShow: 1,
     slidesToScroll: 1,
     nextArrow: <SampleNextArrow />,
@@ -71,19 +79,22 @@ const Homepage: React.FC = () => {
     <div className="carousel-wrapper">
       <Slider {...settings}>
         {slideData.map((slide, index) => (
-          <div key={index}>
-            <img
-              src={slide.image}
-              alt={slide.title}
-              className="carousel-image"
-            />
-            <div className="carousel-content">
-              <h3>{slide.title}</h3>
-              <p>{slide.description}</p>
-              <span>{slide.date}</span>
-              {/* <Link className="link-button" to="/register">
+          <div>
+            <div key={index}>
+              <img
+                src={slide.image}
+                alt={slide.title}
+                style={{ width: "100%", display: "block" }}
+                className="carousel-image"
+              />
+              <div className="carousel-content">
+                <h3>{slide.title}</h3>
+                <p>{slide.description}</p>
+                <span>{slide.date}</span>
+                {/* <Link className="link-button" to="/register">
                             Find your buddy
                         </Link> */}
+              </div>
             </div>
           </div>
         ))}
