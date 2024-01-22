becc118bd78fdfc0c774544090b9865728ba0e82
Sardor Madaminov <sardor0968@gmail.com>
2024-01-22 18:57:46 +0900

    minor changes: added

--- frontend/src/pages/Events/UpcomingDetails.tsx
+++ frontend/src/pages/Events/UpcomingDetails.tsx
@@ -1,18 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
-// challenge: build the shared UI portion of the detals page.  For now, get the data from the request `/api/events/upcoming/:id`
-// and display the van, name, price and description.
 interface EventsDetails {
   id: number;
+  imageUrl: string;
   name: string;
   price: number;
-  imageUrl: string;
   description: string;
 }
 
 export default function UpcomingDetails() {
-  const [eventsDetails, setEventsDetails] = useState<EventsDetails[]>();
+  const [eventsDetails, setEventsDetails] = useState<EventsDetails[]>([]);
 
   const { id } = useParams<{ id: string }>();
 
@@ -26,30 +24,34 @@ export default function UpcomingDetails() {
       try {
         const response = await fetch(urlPath);
         const data = await response.json();
-        console.log(data, " Data fromt the server for Event Details page!");
-        setEventsDetails(data.runners);
+        console.log(data, " Data from the server for Event Details page!");
+        setEventsDetails(data);
       } catch (error) {
-        console.log(error);
+        console.log("Error fetching the data" + error);
       }
     };
     fetchData();
   }, [urlPath]);
 
   // console.log(eventsDetails, "EVENTS DETAILS");
-  const eventsDetailsData = eventsDetails?.map((event) => {
-    // console.log(event.id, "EVENT DETIALS ID IS PRINTED");
-    console.log(eventsDetails, "EVENTS DETAILS");
-    return (
-      <div key={event.id} className="upcoming-event-details">
-        <img src={event.imageUrl} alt={`Pic of ${event.name}`} />
-        <h3> {event.name}</h3>
-        <p> {event.id}</p>
-        <p> ${event.price}</p>
-        <p> {event.description}</p>
-      </div>
+  console.log(eventsDetails, "EVENTS DETAILS Before return statement");
+  const eventsDetailsData =
+    Array.isArray(eventsDetails) && eventsDetails.length > 0 ? (
+      eventsDetails.map((event) => {
+        // console.log(event.id, "EVENT DETIALS ID IS PRINTED");
+        console.log(eventsDetails, "shunchaki");
+        return (
+          <div key={event.id} className="upcoming-event-details">
+            <img src={event.imageUrl} alt={`Pic of ${event.name}`} />
+            <h3> {event.name}</h3>
+            <p> ${event.price}</p>
+            <p> {event.description}</p>
+          </div>
+        );
+      })
+    ) : (
+      <p> No upcoming events found.</p>
     );
-  });
-
   return (
     <div className="container">
       <h1>UpcomingDetails</h1>
