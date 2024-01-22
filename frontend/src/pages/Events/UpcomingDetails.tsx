e64e76f27237f2f3d607561248b6176e722bc646
Sardor Madaminov <sardor0968@gmail.com>
2024-01-23 00:08:34 +0900

    minor changes: added

--- frontend/src/pages/Events/UpcomingDetails.tsx
+++ frontend/src/pages/Events/UpcomingDetails.tsx
@@ -15,16 +15,17 @@ export default function UpcomingDetails() {
   const { id } = useParams<{ id: string }>();
 
   const urlPath = `http://localhost:4000/api/events/upcoming/${id}`;
-  console.log(id, "ID OF THE EVENT");
-
-  // console.log(id, ": params.id");
+  // console.log(id, "ID OF THE EVENT");
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(urlPath);
+        if (!response.ok) {
+          throw new Error(`Http Error! status: ${response.status}`);
+        }
+        // console.log("Raw Response: ", response);
         const data = await response.json();
-        console.log(data, " Data from the server for Event Details page!");
         setEventsDetails(data);
       } catch (error) {
         console.log("Error fetching the data" + error);
@@ -36,13 +37,11 @@ export default function UpcomingDetails() {
   // console.log(eventsDetails, "EVENTS DETAILS");
   console.log(eventsDetails, "EVENTS DETAILS Before return statement");
   const eventsDetailsData =
-    Array.isArray(eventsDetails) && eventsDetails.length > 0 ? (
+    Array.from(eventsDetails) && eventsDetails.length > 0 ? (
       eventsDetails.map((event) => {
-        // console.log(event.id, "EVENT DETIALS ID IS PRINTED");
-        console.log(eventsDetails, "shunchaki");
         return (
           <div key={event.id} className="upcoming-event-details">
-            <img src={event.imageUrl} alt={`Pic of ${event.name}`} />
+            {/* <img src={event.imageUrl} alt={`Pic of ${event.name}`} /> */}
             <h3> {event.name}</h3>
             <p> ${event.price}</p>
             <p> {event.description}</p>
@@ -52,6 +51,7 @@ export default function UpcomingDetails() {
     ) : (
       <p> No upcoming events found.</p>
     );
+
   return (
     <div className="container">
       <h1>UpcomingDetails</h1>
