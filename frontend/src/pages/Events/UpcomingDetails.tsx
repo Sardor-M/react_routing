de92223d6bac7fa772fe70ff8e7a5509ca214e67
Sardor-M <sardor0968@gmail.com>
2024-02-26 15:42:14 +0900

    merge conflict fixed

--- frontend/src/pages/Events/UpcomingDetails.tsx
+++ frontend/src/pages/Events/UpcomingDetails.tsx
@@ -5,7 +5,7 @@ import { Events } from "../../types";
 
 export default function UpcomingDetails() {
   // saving the fetched data as an object
-  const [eventDetails, setEventsDetails] = useState<Events>;
+  const [eventDetails, setEventsDetails] = useState<Events | null>(null);
 
   const { id } = useParams<{ id: string }>();
   const urlPath = `http://localhost:4000/api/events/upcoming/${id}`;
