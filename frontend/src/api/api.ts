097ee0731183c56b8712e5aa36f985087088b4d8
Sardor Madaminov <sardor0968@gmail.com>
2024-03-04 22:15:31 +0900

    space refactoring: added

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,41 +1,40 @@
 import { GetEvent } from "../types";
 
-
 async function fetchData(url: string): Promise<any> {
-    const response = await fetch(url);
-    
-    if (!response.ok){
-        throw {
-            message: "Error from data server is thrown by Runner's side",
-            statusText: response.statusText,
-            status: response.status,
-        }
-    }
-    return response.json();
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw {
+      message: "Error from data server is thrown by Runner's side",
+      statusText: response.statusText,
+      status: response.status,
+    };
+  }
+  return response.json();
 }
 
-export  async function getEvents( ) {
-    return fetchData("http://localhost:4000/api/runners");
+export async function getEvents() {
+  return fetchData("http://localhost:4000/api/runners");
 }
 
 export async function getEventDetail(id: string) {
-    return fetchData(`http://localhost:4000/api/runners/${id}`);
-
+  return fetchData(`http://localhost:4000/api/runners/${id}`);
 }
 
+export async function getEventDetails(id: GetEvent) {
+  const url = id
+    ? `http://localhost:4000/api/events/upcoming${id}`
+    : "http://localhost:4000/api/events/upcoming";
+  const response = await fetch(url);
 
-export async function getEventDetails(id : GetEvent) {
-    const url = id ? `http://localhost:4000/api/events/upcoming${id}` : "http://localhost:4000/api/events/upcoming";
-    const response = await fetch(url);
-    
-    if(!response.ok){
-        throw {
-            message: "Error fetching data from the server !",
-            statusText: response.statusText,
-            status: response.status,
-        }
-    }
-    const data = await response.json();
-    return data;
-    // const response = await fetch ("/events/upcoming/${id}")
+  if (!response.ok) {
+    throw {
+      message: "Error fetching data from the server !",
+      statusText: response.statusText,
+      status: response.status,
+    };
+  }
+  const data = await response.json();
+  return data;
+  // const response = await fetch ("/events/upcoming/${id}")
 }
