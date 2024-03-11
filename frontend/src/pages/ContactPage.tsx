7811d80d6394ca4679d16067f2ac308e2ac2b545
Sardor Madaminov <sardor0968@gmail.com>
2024-03-11 22:56:28 +0900

    fixed some styling: contactpage

--- frontend/src/pages/ContactPage.tsx
+++ frontend/src/pages/ContactPage.tsx
@@ -1,103 +1,93 @@
 import styled from "styled-components";
 import React from "react";
-import {ContactPageProps} from "../types";
-import {getEvents} from "../api/api";
+// import {ContactPageProps} from "../types";
+import { getEvents } from "../api/api";
 
+const ContactPageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 60px;
+  margin: 10px;
+`;
+const InputField = styled.input`
+  width: 50%;
+  padding: 13px;
+  margin: 15px 0;
+  border: none;
+  border-radius: 5px;
+`;
 
-const ContactPageElement = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-`
-const NameInputField = styled.input`
-    width: 90%;
-    padding: 13px;
-    margin: 15px 0;
-    border: none;
-    border-radius: 5px;
-`
-const EmailInputField = styled.input`
-    width: 90%;
-    padding: 13px;
-    margin: 15px;
-    border: none;
-    border-radius: 5px;
-`
-
-const MessageInputField = styled.input`
-    width: 90%;
-    padding: 13px;
-    margin: 15px;
-    border: none;
-    border-radius: 5px;
-    color: #f5a646;
-`
-
-const CityInputField = styled.input`
-    width: 90%;
-    padding: 13px;
-    margin: 15px;
-    border: none;
-    border-radius: 5px;
-    color: #f5a646;
-`
+const MessageField = styled.textarea`
+  width: 50%;
+  padding: 30px;
+  margin: 15px 0;
+  border: none;
+  border-radius: 5px;
+  height: 200px;
+`;
 
 const SubmitButton = styled.button`
-    width: 90%;
-    padding: 13px;
-    margin: 15px;
-    border: none;
-    border-radius: 5px;
-    color: #f5a646;
-    background-color: #f5a646;
-    font-size: 18px;
-`
+  width: 50%;
+  padding: 13px;
+  margin: 15px;
+  border: none;
+  border-radius: 5px;
+  color: #0d0d0d;
+  background-color: #f5a646;
+  font-size: 18px;
+`;
 
 export function loader() {
-    return getEvents();
+  return getEvents();
 }
 
 export default function ContactPage() {
-    const [inputData, setInputData] = React.useState({name: "", email: "", city: "", message: ""});
+  const [inputData, setInputData] = React.useState({
+    name: "",
+    email: "",
+    city: "",
+    message: "",
+  });
 
-    const handleChange = (name: string) => {
-        return (e: React.ChangeEvent <HTMLInputElement>) => {
-            setInputData({
-                ...inputData,
-                [name]: e.target.value
-            })
-        }
-    }
+  const handleChange = (name: string) => {
+    return (e: React.ChangeEvent<HTMLInputElement>) => {
+      setInputData({
+        ...inputData,
+        [name]: e.target.value,
+      });
+    };
+  };
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-    return (
-        <>
-            <h1>Contact Us</h1>
-            <ContactPageElement>
-                <NameInputField
-                    type="input"
-                    placeholder={"Example: Steve"}
-                    value={inputData?.email}
-                    onChange={handleChange("name")}
-                >
-                    Your Name
-                </NameInputField>
-                <EmailInputField>
-                    Email address
-                </EmailInputField>
-                <CityInputField>
-                    City Name
-                </CityInputField>
-                <MessageInputField>
-                    Message
-                </MessageInputField>
-                <SubmitButton
-                    type="submit"
-                >
-                    Submit
-                </SubmitButton>
-            </ContactPageElement>
-        </>
-    );
+    // submit the form data should be defined
+  };
+  return (
+    <>
+      <h1>Contact Us</h1>
+      <ContactPageContainer>
+        <InputField
+          type="text"
+          placeholder={"Your Name"}
+          value={inputData?.email}
+          onChange={handleChange("name")}
+        />
+        <InputField
+          type="email"
+          placeholder="Email address"
+          value={inputData?.email}
+          onChange={handleChange("email")}
+        />
+        <MessageField
+          placeholder="Your Message"
+          value={inputData?.message}
+          onChange={handleSubmit("message")}
+        />
+        <SubmitButton type="submit">Submit</SubmitButton>
+      </ContactPageContainer>
+    </>
+  );
 }
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,127 +1,124 @@
-import React, {useState} from "react";
-import {useLoaderData} from "react-router";
+import React, { useState } from "react";
+import { useLoaderData } from "react-router";
 import styled from "styled-components";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 type RequestType = {
-    url: React.ReactNode;
+  url: React.ReactNode;
 };
 
 const LoginInput = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: flex;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex;
+`;
 
 const StyledForm = styled.form`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    padding: 60px;
-    margin: 20px;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 60px;
+  margin: 20px;
+`;
 
 const LoginInputData = styled.input`
-    width: 180%;
-    padding: 13px;
-    margin: 15px 0;
-    border: none;
-    border-radius: 5px;
-    box-shadow: 0 0 5px rgba(0, 62, 213, 0.1);
-    outline: none;
+  width: 180%;
+  padding: 13px;
+  margin: 15px 0;
+  border: none;
+  border-radius: 5px;
+  box-shadow: 0 0 5px rgba(0, 62, 213, 0.1);
+  outline: none;
 
-    &:hover {
-        box-shadow: 0 0 5px rgba(155, 0, 0, 0.3);
-    }
-`
+  &:hover {
+    box-shadow: 0 0 5px rgba(155, 0, 0, 0.3);
+  }
+`;
 
 const LoginButton = styled.button`
-    width: 180%;
-    padding: 9px;
-    margin: 15px;
-    border: none;
-    background-color: #f5a646;
-    color: white;
-    font-size: 18px;
-`
+  width: 180%;
+  padding: 9px;
+  margin: 15px;
+  border: none;
+  background-color: #f5a646;
+  color: #000000;
+  font-size: 18px;
+`;
 const SignUpLink = styled.p`
-    color: #e17654;
-    font-size: 14px;
-    margin-top: 10px;
-`
+  color: #e17654;
+  font-size: 14px;
+  margin-top: 10px;
+`;
 
 // accessing the Request object which is a native browser object
 // we are extracting  a new URL from the request object
-export async function loader({request}: { request: Request }) {
-    return new URL(request.url).searchParams.get("message");
+export async function loader({ request }: { request: Request }) {
+  return new URL(request.url).searchParams.get("message");
 }
 
 export default function LoginPage() {
-    const messageData = useLoaderData() as string | "";
+  const messageData = useLoaderData() as string | "";
 
-    const [loginData, setLoginData] = useState({
-        email: "",
-        password: "",
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(loginData);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLoginData({
+      ...loginData,
+      [e.target.name]: e.target.value,
     });
+  };
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        console.log(loginData);
-    };
+  const handleSignUp = (e: React.FormEvent) => {
+    const link = document.links[0];
+    console.log(link);
+  };
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setLoginData({
-            ...loginData,
-            [e.target.name]: e.target.value,
-        });
-    };
-
-    const handleSignUp = (e: React.FormEvent) => {
-        const link = document.links[0];
-        console.log(link);
-    }
-
-    return (
-        <div className="container">
-            <h1> Sign in to your account</h1>
-            {messageData && <h3 className="login-text">{messageData} </h3>}
-            <StyledForm onSubmit={handleSubmit}>
-                <LoginInput>
-                    <LoginInputData
-                        type="email"
-                        name="email"
-                        placeholder="Email"
-                        required
-                        value={loginData?.email}
-                        onChange={handleChange}
-                    />
-                    <LoginInputData
-                        type="pasword"
-                        name="password"
-                        placeholder="password"
-                        required
-                        value={loginData?.password}
-                        onChange={handleChange}
-                    />
-                    <LoginButton
-                        type="submit"
-                        onClick={handleSubmit}
-                    >
-                        Login
-                    </LoginButton>
-                    <SignUpLink
-
-                        onClick={handleSignUp}
-                    >
-                        Not Registered yet ? {" "}
-                        <Link to={'/signup'} style={{color: "inherit", textDecoration: 'underline'}}>
-                            Sign Up
-                        </Link>
-                    </SignUpLink>
-                </LoginInput>
-            </StyledForm>
-        </div>
-    );
+  return (
+    <div className="container">
+      <h1> Sign in to your account</h1>
+      {messageData && <h3 className="login-text">{messageData} </h3>}
+      <StyledForm onSubmit={handleSubmit}>
+        <LoginInput>
+          <LoginInputData
+            type="email"
+            name="email"
+            placeholder="Email"
+            required
+            value={loginData?.email}
+            onChange={handleChange}
+          />
+          <LoginInputData
+            type="pasword"
+            name="password"
+            placeholder="Password"
+            required
+            value={loginData?.password}
+            onChange={handleChange}
+          />
+          <LoginButton type="submit" onClick={handleSubmit}>
+            Login
+          </LoginButton>
+          <SignUpLink onClick={handleSignUp}>
+            Not Registered yet ?{" "}
+            <Link
+              to={"/signup"}
+              style={{ color: "inherit", textDecoration: "underline" }}
+            >
+              Sign Up
+            </Link>
+          </SignUpLink>
+        </LoginInput>
+      </StyledForm>
+    </div>
+  );
 }
